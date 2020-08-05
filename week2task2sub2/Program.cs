a9a00e9f8b20a2b17916eb0d55878f50fc1da54c
Peter713 <plaszuk@onet.eu>
2020-08-05 21:30:37 +0200

    Secon commit week2task4sub2
    Reverse order in comparison with declaration, Mistake in the file  name.

---
+++ week2task2sub2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace week2task2sub2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//variable with one letter
+            char firstLetter = 'a';
+            char secondLetter = 'b';
+            char thirdLetter = 'c';
+
+            Console.WriteLine($"Reverse order: {thirdLetter}, {secondLetter}, {firstLetter}");
+            Console.ReadKey();
+        }
+
+      
+    }
+}
