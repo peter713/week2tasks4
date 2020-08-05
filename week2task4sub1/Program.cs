c6a8534ec27db1a2317e7c131b4dee96a0346a7c
Peter713 <plaszuk@onet.eu>
2020-08-05 21:18:14 +0200

    First commint week2task4sub1
    Declaration of the various data type

---
+++ week2task4sub1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace week2task4sub1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//decalratation
+            string Name;
+            string Surname;
+            int Age;
+            Gender MaleFemale;
+            long PersonalId;
+            long WorkerId;
+        }
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+    }
+}
