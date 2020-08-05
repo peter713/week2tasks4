959834acf0ac800d6f0fcd248c3d101fcd2dfc2e
Peter713 <plaszuk@onet.eu>
2020-08-05 22:58:43 +0200

    Fourth commit week2task4sub4
    Adding different variables

---
+++ week2task4sub4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace week2task4sub4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//creation different variables
+            int myInt;
+            string myString;
+            double myDouble;
+
+            myInt = 10;
+            myString = "Szkoła Dotneta";
+            myDouble = 12.5;
+
+            //second way
+            //int myInt =10;
+            //string myString = "Szkoła Dotneta";
+            //double myDouble = 12.5;
+
+        }
+    }
+}
